3880a5ee01852dda16d8306675d8d76d6d07b87e
SmendowskiMateusz <smendowski@student.agh.edu.pl>
2021-04-14 18:52:23 +0200

    Data Model usage

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
 app = FastAPI()
+app.counter = 0
+
+
+class HelloResp(BaseModel):
+    msg: str
+
 
 @app.get("/")
 def root():
 	return {"message": "Hello World"}
 
+
+@app.get("/hello/{name}")
+def hello_name_view(name: str):
+    return f"Hello {name}"
+
+
+@app.get('/counter')
+def counter():
+    app.counter += 1
+    return app.counter
+
+
+@app.get("/helloModel/{name}", response_model=HelloResp)
+async def read_item(name: str):
+    return HelloResp(msg=f"Hello {name}")
--- tests.py
+++ tests.py
@@ -2,6 +2,8 @@ from fastapi.testclient import TestClient
 
 from main import app
 
+import pytest
+
 client = TestClient(app)
 
 
@@ -9,3 +11,28 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+@pytest.mark.parametrize("name", ["Zenek", "Marek", "Alojzy Niezdąży"])
+def test_hello_name(name):
+    response = client.get(f"/hello/{name}")
+    assert response.status_code == 200
+    assert response.text == f'"Hello {name}"'
+
+
+def test_counter():
+    response = client.get(f"/counter")
+    assert response.status_code == 200
+    assert response.text == "1"
+    # 2nd Try
+    response = client.get(f"/counter")
+    assert response.status_code == 200
+    assert response.text == "2"
+
+
+def test_read_item(name):
+    response = client.get(f"/helloModel/{name}")
+    assert response.status_code == 200
+    print(response.text)
+    print(type(response.text))
+    assert response.text.msg == f'"Hello {name}"'
