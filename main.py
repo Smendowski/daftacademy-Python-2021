82929837943487ebeb4129986a779720b79a5d6b
SmendowskiMateusz <smendowski@student.agh.edu.pl>
2021-04-16 15:35:51 +0200

    Daftacademy Task 1.3 Solution

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Request, Response, status
 
 from pydantic import BaseModel
 
+import hashlib
+
 app = FastAPI()
 app.counter = 0
 
@@ -14,24 +16,30 @@ class HelloResp(BaseModel):
 def root():
 	return {"message": "Hello world!"}
 
-
 @app.api_route("/method", methods=['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'])
 def handle_method(request: Request, response: Response):
     if request.method == 'POST':
         response.status_code = status.HTTP_201_CREATED
     return {"method": f"{request.method}"}
 
+@app.get("/auth")
+async def authorize_password(response: Response, password: str = '', password_hash: str = ''):
+    if password != '' and password_hash != '':
+        hash_checked = hashlib.sha512(password.encode()).hexdigest()
+        if password_hash == hash_checked:
+            response.status_code = status.HTTP_204_NO_CONTENT
+            return    
+    response.status_code = status.HTTP_401_UNAUTHORIZED
+    return
 
 @app.get("/hello/{name}")
 def hello_name_view(name: str):
     return f"Hello {name}"
 
-
 @app.get('/counter')
 def counter():
     app.counter += 1
     return app.counter
-
 
 @app.get("/hello/{name}", response_model=HelloResp)
 async def read_item(name: str):
--- tests.py
+++ tests.py
@@ -12,20 +12,25 @@ def test_read_main():
     assert response.status_code == 200
     assert response.json() == {"message": "Hello world!"}
 
-
 @pytest.mark.parametrize("method", ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'])
 def test_http_method(method):
     response = client.request(method=method, url='/method')
     assert response.status_code == 200 if method != 'POST' else response.status_code == 201
     assert response.json() == {"method": f"{method}"}
 
+def test_password():
+    response = client.get(f"/auth?password=haslo&password_hash=013c6889f799cd986a735118e1888727d1435f7f623d05d58c61bf2cd8b49ac90105e5786ceaabd62bbc27336153d0d316b2d13b36804080c44aa6198c533215")
+    assert response.status_code == 204
+    response = client.get(f"/auth?password=haslo&password_hash=f34ad4b3ae1e2cf33092e2abb60dc0444781c15d0e2e9ecdb37e4b14176a0164027b05900e09fa0f61a1882e0b89fbfa5dcfcc9765dd2ca4377e2c794837e091")
+    assert response.status_code == 401
+    response = client.get(f"/auth")
+    assert response.status_code == 401
 
 @pytest.mark.parametrize("name", ["Zenek", "Marek", "Alojzy Niezdąży"])
 def test_hello_name(name):
     response = client.get(f"/hello/{name}")
     assert response.status_code == 200
     assert response.text == f'"Hello {name}"'
-
 
 def test_counter():
     response = client.get(f"/counter")
